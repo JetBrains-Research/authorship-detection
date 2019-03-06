15b2d17ce0b094c51c0cfdfbb6f315fa37240505
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-03-07 01:52:37 +0300

    Add path retrieval

--- mining/src/main/kotlin/mining/Main.kt
+++ mining/src/main/kotlin/mining/Main.kt
@@ -1,10 +1,28 @@
 package mining
 
 import mining.joern.parseJoernAst
+import miningtool.common.toPathContext
+import miningtool.parse.antlr.joern.CppParser
+import miningtool.paths.PathMiner
+import miningtool.paths.PathRetrievalSettings
+import miningtool.paths.storage.VocabularyPathStorage
 import java.io.File
 
 fun main(args: Array<String>) {
-    val root = parseJoernAst(File("../parsed/datasets/cppSample/1.cpp/nodes.csv"),
-            File("../parsed/datasets/cppSample/1.cpp/edges.csv"))
-    root?.prettyPrint()
+    val folder = "../parsed/datasets/cppSample/"
+
+    val miner = PathMiner(PathRetrievalSettings(8, 3))
+    val storage = VocabularyPathStorage()
+
+    File(folder).walkTopDown().filter { it.path.endsWith(".cpp") && it.isDirectory }.forEach { directory ->
+        val nodesFile = File(directory, "nodes.csv")
+        val edgesFile = File(directory, "edges.csv")
+
+        val node = parseJoernAst(nodesFile, edgesFile) ?: return@forEach
+        val paths = miner.retrievePaths(node)
+
+        storage.store(paths.map { toPathContext(it) }, entityId = directory.path)
+    }
+
+    storage.save("../processed/cppSample")
 }
