2fe53467c6aff000b590c0d86dcfc4bf6c1e22b0
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-03-06 19:48:40 +0300

    Parse joern's output into format compatible with miningtool

--- mining/src/main/kotlin/mining/Main.kt
+++ mining/src/main/kotlin/mining/Main.kt
@@ -1,5 +1,10 @@
 package mining
 
+import mining.joern.parseJoernAst
+import java.io.File
+
 fun main(args: Array<String>) {
-    println("Hello, world!")
+    val root = parseJoernAst(File("../parsed/datasets/cppSample/1.cpp/nodes.csv"),
+            File("../parsed/datasets/cppSample/1.cpp/edges.csv"))
+    root?.prettyPrint()
 }
