42491f0050e68945378830dd0687a9e1a7e9a15e
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2020-01-14 14:13:30 +0300

    Drop old miner

--- mining/process_file.py
+++
@@ -1,8 +0,0 @@
-import ast
-import ruamel.yaml
-import sys
-
-filename = sys.argv[1]
-f = open(filename, 'r')
-tree = ast.parse(f.read(), filename)
-ruamel.yaml.dump(tree, open(sys.argv[2], 'w'), default_flow_style=False)
